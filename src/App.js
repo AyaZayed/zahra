f69e78e9b66eba8f96759e81fe32683c2bad712b
Aya Zayed <ayazayed@std.mans.edu.eg>
2023-06-06 02:55:52 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import './css/App.css'
 
 export default function App() {
   return (
     <>
       <Routes>
-        {/* <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Home />} />
-        </Route> */}
+        <Route path="/" element={<Home />} />
       </Routes>
     </>
   )
---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../assets/logo/zahra.png'
+
+export default function Navbar() {
+    return (
+        <>
+            <a href="#navbar" id="menu-toggle" className="menu-toggle" aria-label="Open  menu">
+                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM64 256c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H96c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" /></svg>
+            </a>
+            <nav className='navbar' id="navbar" aria-label="navbar menu">
+                <a href="#menu-toggle" id="menu-close" className="menu-close" aria-label="Close  menu">
+                    x
+                </a>
+                <Link to='/' exact='true' className='logo'>
+                    <img src={logo} alt="zahra name and logo" />
+                </Link>
+                <ul>
+                    <li><Link to='/' exact='true'>الصفحة الرئيسية</Link></li>
+                    <li><Link to='/contact'>تواصل معنا </Link></li>
+                    <li><Link to='/login'>تسجيل الدخول</Link></li>
+                    <li><Link to='/login'><button>انضم الآن</button></Link></li>
+                </ul>
+                <a href="#main-menu-toggle" className="backdrop" tabIndex="-1" hidden></a>
+            </nav>
+        </>
+    )
+}
