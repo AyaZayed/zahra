ae706850dcf7509aac604711ad206fe480451cc5
Aya Zayed <ayazayed@std.mans.edu.eg>
2023-06-06 01:11:12 +0300

    file structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router-dom'
+
+export default function App() {
+  return (
+    <>
+      <Routes>
+        {/* <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+        </Route> */}
+      </Routes>
+    </>
+  )
+}
