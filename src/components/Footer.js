fe78c011952666516fa3d1fedcd4a189dbf98873
Aya Zayed <ayazayed@std.mans.edu.eg>
2023-06-06 04:04:06 +0300

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,45 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../assets/logo/zahra.png'
+import email from '../assets/icons/email.png'
+import facebook from '../assets/icons/facebook-footer.png'
+import instagram from '../assets/icons/instagram.png'
+import twitter from '../assets/icons/twitter.png'
 
 export default function Footer() {
     return (
-        <div></div>
+        <footer className="footer">
+            <div className="container logo">
+                <Link to='/' exact='true' className='logo'>
+                    <img src={logo} alt="zahra name and logo" />
+                </Link>
+            </div>
+            <div className="container">
+                <h6>من نحن</h6>
+                <h6>آراء المستخدمين</h6>
+            </div>
+            <div className="container">
+                <h6>سياسة الخصوصية</h6>
+                <h6>شروط الاستخدام</h6>
+            </div>
+            <div className="container">
+                <h6>تواصل معنا</h6>
+                <h6>للإعلان</h6>
+            </div>
+            <div className="container socials">
+                <Link to='#' exact='true' className='mail'>
+                    <img src={email} alt="link to zahra email" />
+                </Link>
+                <Link to='#' exact='true' className='facebook'>
+                    <img src={facebook} alt="link to zahra facebook" />
+                </Link>
+                <Link to='#' exact='true' className='instagram'>
+                    <img src={instagram} alt="link to zahra instagram" />
+                </Link>
+                <Link to='#' exact='true' className='twitter'>
+                    <img src={twitter} alt="link to zahra twitter" />
+                </Link>
+            </div>
+        </footer>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,14 @@ export default function Home() {
     return (
         <div>
             <Navbar />
+            <section className="welcome">
+                <div className="welcome_text">
+                    <h1>مرحباً بك في <img src='../assets/logo/logo.png' alt='zahra' /> للأطباء</h1>
+                </div>
+                <div className="welcome_image"></div>
+            </section>
+            <section className="services"></section>
+            <section className="subscriptions"></section>
             <Footer />
         </div>
     )
