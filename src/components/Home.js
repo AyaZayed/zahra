22da1e5423b2ab77e0dfe0793a23af1d603f102f
Aya Zayed <ayazayed@std.mans.edu.eg>
2023-06-07 00:21:50 +0300

    landing page welcome section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,29 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import logo from '../assets/logo/logo.png'
+import welcome from '../assets/illustorations/landing-doctors.png'
+import { NavLink } from 'react-router-dom'
 
 export default function Home() {
     return (
-        <div>
+        <>
             <Navbar />
-            <section className="welcome">
-                <div className="welcome_text">
-                    <h1>مرحباً بك في <img src='../assets/logo/logo.png' alt='zahra' /> للأطباء</h1>
-                </div>
-                <div className="welcome_image"></div>
-            </section>
-            <section className="services"></section>
-            <section className="subscriptions"></section>
+            <main className='home-page'>
+                <section className="welcome">
+                    <div className="welcome-text">
+                        <h1>مرحباً بك في <span><img src={logo} alt='zahra' /></span> للأطباء</h1>
+                        <p>أول موقع عربى لمساعدتك كطبيب فى تشخيص مرض سرطان الثدى </p>
+                        <NavLink to='login' exact='true'><button>انضم الآن</button></NavLink>
+                    </div>
+                    <div className="welcome-image">
+                        <img src={welcome} alt='image of three doctors in lab coats' />
+                    </div>
+                </section>
+                <section className="services"></section>
+                <section className="subscriptions"></section>
+            </main>
             <Footer />
-        </div>
+        </>
     )
 }
