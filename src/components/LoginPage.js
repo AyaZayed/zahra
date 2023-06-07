5e6b5833858d00cb79ba87a7360bad55b2799265
Aya Zayed <ayazayed@std.mans.edu.eg>
2023-06-07 23:59:20 +0300

    login page structure

---
+++ src/components/LoginPage.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useState } from 'react'
+import loginImage from '../assets/illustorations/login-page.png'
+import Login from './Login'
+import Signup from './Signup'
+
+export default function LoginPage() {
+    const [newUser, setNewUser] = useState(false)
+    return (
+        <div className='login-page'>
+            <div className='login-image'>
+                <img src={loginImage} alt="login" />
+            </div>
+            <div className='login-form'>
+                <div className='login-tabs'>
+                    <button className={newUser ? 'login-btn active' : 'login-btn'} onClick={() => setNewUser(true)}>
+                        حساب جديد</button>
+                    <button className={newUser ? 'login-btn' : 'login-btn active'} onClick={() => setNewUser(false)}>تسجيل الدخول</button>
+                </div>
+                {
+                    newUser ? <Signup /> : <Login />
+                }
+            </div>
+
+        </div>
+    )
+}
