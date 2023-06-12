4de39e613139ec35c122e33308f0d34aeaec9055
Aya Zayed <ayazayed@std.mans.edu.eg>
2023-06-12 04:18:52 +0300

    dashboard page styling

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,27 +6,29 @@ import appointment from '../assets/icons/appointment.png'
 import medicalReport from '../assets/icons/medical-reports.png'
 import chat from '../assets/icons/chat.png'
 import diagnosis from '../assets/icons/diagnosis.png'
+import location from '../assets/icons/location.png'
+import time from '../assets/icons/time.png'
 import { Link } from 'react-router-dom'
 
 export default function Dashboard() {
     const [user, setUser] = useState('ماهر')
     const [appointments, setAppointments] = useState([{
         patient: 'كريم الموجي',
-        patientImage: 'https://res.cloudinary.com/dcfrq5i2y/image/upload/v1625327505/samples/people/smiling-man.jpg',
+        patientImage: 'https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=876&q=80',
         location: 'العيادة',
         date: 'الأحد 8 يناير',
         time: '10:00 صباحاََ',
     },
     {
         patient: 'كريم الموجي',
-        patientImage: 'https://res.cloudinary.com/dcfrq5i2y/image/upload/v1625327505/samples/people/smiling-man.jpg',
+        patientImage: 'https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=876&q=80',
         location: 'العيادة',
         date: 'الأحد 8 يناير',
         time: '10:00 صباحاََ',
     },
     {
         patient: 'كريم الموجي',
-        patientImage: 'https://res.cloudinary.com/dcfrq5i2y/image/upload/v1625327505/samples/people/smiling-man.jpg',
+        patientImage: 'https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=876&q=80',
         location: 'العيادة',
         date: 'الأحد 8 يناير',
         time: '10:00 صباحاََ',
@@ -35,56 +37,62 @@ export default function Dashboard() {
     return (
         <>
             <Navbar landing={false} />
-            <main className='dashboard-page'>
+            <main className='dashboard'>
                 <div className='features'>
                     <h6>مرحباََ , {user}.</h6>
                     <p>
                         نتمنى لك يوماََ لطيفاََ ^-^</p>
-                    <div className='features-list'>
-                        <Link to='/dashboard/appointments' exact='true'>
-                            <div className='features-item'>
-                                <img src={appointment} alt='appointment timetable' />
-                                <h6>الحجوزات</h6>
-                            </div>
-                        </Link>
-                        <Link to='/dashboard/patients' exact='true'>
-                            <div className='features-item'>
-                                <img src={medicalReport} alt='medical report' />
-                                <h6>قائمة المرضى</h6>
-                            </div>
-                        </Link>
-                        <Link to='/dashboard/chat' exact='true'>
-                            <div className='features-item'>
-                                <img src={chat} alt='chat' />
-                                <h6>المحادثات</h6>
-                            </div>
-                        </Link>
-                        <Link to='/dashboard/diagnosis' exact='true'>
-                            <div className='features-item'>
-                                <img src={diagnosis} alt='diagnosis tool' />
-                                <h6>أداة الفحص</h6>
-                            </div>
-                        </Link>
+                    <div className='features-wrapper'>
+                        <div className='features-list'>
+                            <Link to='/dashboard/appointments' exact='true'>
+                                <div className='features-item'>
+                                    <img src={appointment} alt='appointment timetable' />
+                                    <h6>الحجوزات</h6>
+                                </div>
+                            </Link>
+                            <Link to='/dashboard/patients' exact='true'>
+                                <div className='features-item'>
+                                    <img src={medicalReport} alt='medical report' />
+                                    <h6>قائمة المرضى</h6>
+                                </div>
+                            </Link>
+                            <Link to='/dashboard/chat' exact='true'>
+                                <div className='features-item'>
+                                    <img src={chat} alt='chat' />
+                                    <h6>المحادثات</h6>
+                                </div>
+                            </Link>
+                            <Link to='/dashboard/diagnosis' exact='true'>
+                                <div className='features-item'>
+                                    <img src={diagnosis} alt='diagnosis tool' />
+                                    <h6>أداة الفحص</h6>
+                                </div>
+                            </Link>
+                        </div>
                     </div>
                 </div>
                 <div className='appointments'>
-                    <h6>مواعيد قريبة</h6>
-                    <div className='appointments-list'>
-                        {appointments.map((appointment, index) => {
-                            return (
-                                <div className='appointments-item' key={index}>
-                                    <div className='patient-info'>
-                                        <img src={appointment.patientImage} alt='patient' />
-                                        <h6>{appointment.patient}</h6>
+                    <div className='heading-text'>
+                        <h6>مواعيد قريبة</h6>
+                        <Link to='/dashboard/appointments' exact='true'>المزيد</Link>
+                    </div>
+                    <div className='appointments-wrapper'>
+                        <div className='appointments-list'>
+                            {appointments.map((appointment, index) => {
+                                return (
+                                    <div className='appointments-item' key={index}>
+                                        <div className='patient-info'>
+                                            <img src={appointment.patientImage} alt='patient' />
+                                            <h6>{appointment.patient}</h6>
+                                        </div>
+                                        <div className='appointments-item-text'>
+                                            <small><img src={location} alt='location pin' />{appointment.location}</small>
+                                            <small><img src={time} alt='clock' />{appointment.date} {appointment.time}</small>
+                                        </div>
                                     </div>
-                                    <div className='appointments-item-text'>
-                                        <small>{appointment.location}</small>
-                                        <small>{appointment.date}</small>
-                                        <small>{appointment.time}</small>
-                                    </div>
-                                </div>
-                            )
-                        })}
+                                )
+                            })}
+                        </div>
                     </div>
                 </div>
             </main>
